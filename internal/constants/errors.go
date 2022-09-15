ae00eee76b3ac394bbff9ca2eeb6711e6485a65b
hinccvi <alexander.thong.wh@gmail.com>
2022-09-15 22:36:37 +0700

    update error constant

--- internal/constants/errors.go
+++ internal/constants/errors.go
@@ -8,24 +8,23 @@ import (
 const (
 	MsgSystemError    = "system error"
 	MsgBadRequest     = "invalid input"
-	MsgRequestTimeout = "Request timeout"
+	MsgRequestTimeout = "request timeout"
 )
 
 var (
-	ErrCustomErr           = errors.New("custom error")
+	ErrMaxAttempt          = errors.New("max attempt reached")
 	ErrInvalidCredentials  = errors.New("incorrect username or password")
 	ErrConditionNotFulfil  = errors.New("condition not fulfil")
 	ErrInvalidRefreshToken = errors.New("invalid refresh token")
 	ErrInvalidJwt          = errors.New("invalid token")
-	ErrMaxAttempt          = errors.New("max attempt reached")
 
 	ErrorStatusCodeMaps = map[error]int{
 		// Business logic error
-		ErrCustomErr:           http.StatusBadRequest,
+		ErrMaxAttempt: http.StatusBadRequest,
+
 		ErrInvalidCredentials:  http.StatusBadRequest,
 		ErrConditionNotFulfil:  http.StatusBadRequest,
 		ErrInvalidRefreshToken: http.StatusBadRequest,
 		ErrInvalidJwt:          http.StatusBadRequest,
-		ErrMaxAttempt:          http.StatusBadRequest,
 	}
 )
